5c2711ab37b9a4c388bbc6fe3f392ba10b4e1c0c
mazol024 <38819166+mazol024@users.noreply.github.com>
2021-08-04 19:37:07 +1200

    Update Rational.kt
    infix functions added

--- Rationals/Task/src/rationals/Rational.kt
+++ Rationals/Task/src/rationals/Rational.kt
@@ -3,9 +3,7 @@ import java.math.BigInteger
 
 class Rational (numerator:Int,denominator:Int){
     val rational:Pair<Int,Int> = Pair(numerator,denominator)
-    //val rvalue = rational.first/rational.second
     val normalized: Pair<Int,Int> = normalizeIt()
-
     private fun normalizeIt():Pair<Int,Int> {
 
         //var normalized
@@ -26,25 +24,51 @@ class Rational (numerator:Int,denominator:Int){
         return "${sign1}" +
                 "${Math.abs(rational.first)}/${Math.abs(rational.second)}  " +
                 "(Normalized version: ${sign1}${Math.abs(normalized.first)}" +
-                "${if(Math.abs(normalized.second) != 1) "/".plus(Math.abs(normalized.second)) 
+                "${if(Math.abs(normalized.second) != 1) "/".plus(Math.abs(normalized.second))
                 else ""})"
     }
+
 }
+infix  fun Int.divBy(b:Int):Rational = Rational(this,b)
+infix  fun Long.divBy(b:Long):Rational =Rational(this.toInt(),b.toInt())
+infix  fun BigInteger.divBy(b:BigInteger):Rational =Rational(this.toInt(),b.toInt())
+
+fun String.toRational():Rational {
+    var pair = this.split("/")
+    var outp: Rational
+    when {
+        pair.size == 0 -> outp = Rational(0,0)
+        pair.size ==1 -> outp = Rational(pair[0].toInt(),1)
+        else -> outp = Rational(pair[0].toInt(),pair[1].toInt())
+    }
+    return outp
+}
+
+infix fun Rational.plusFun(b:Rational): Rational = Rational(this.rational.first*b.rational.second + this.rational.second*b.rational.first,
+    this.rational.second*b.rational.second)
 
 
 fun main() {
+
     val a:Rational = Rational(117,1098)
     println(a.toString() )
-    val b:Rational = Rational(122,-1)
+    val b:Rational = Rational(2,1)
     println(b.toString() )
-    val c:Rational = Rational(42,1)
+    val c:Rational = Rational(-2,4)
     println(c.toString() )
     val d:Rational = Rational(-21,105)
     println(d.toString() )
-//    val half = 1 divBy 2
-//    val third = 1 divBy 3
-//
-//    val sum: Rational = half + third
+
+   val half = 1 divBy 2
+   val third = 1 divBy 3
+
+    println("half = $half third = $third")
+    val sum: Rational = half plusFun  third
+    println("Sum $half plus $third = $sum ")
+    println(5 divBy 6)
+    println(" String notaion 3/33 : ${"3/33".toRational()}")
+    println(" String notaion 23/1 : ${"23/1".toRational()}")
+    println(" String notaion 123 : ${"123".toRational()}")
 //    println(5 divBy 6 == sum)
 //
 //    val difference: Rational = half - third
