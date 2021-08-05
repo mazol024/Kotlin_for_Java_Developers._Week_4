7a9897be60c2d32e5d2dbf99e67d2e3cd3748105
mazol024 <38819166+mazol024@users.noreply.github.com>
2021-08-05 18:16:34 +1200

    needs range only

--- Rationals/Task/src/rationals/Rational.kt
+++ Rationals/Task/src/rationals/Rational.kt
@@ -1,5 +1,4 @@
 package rationals
-import java.math.BigDecimal
 import java.math.BigInteger
 import java.util.NoSuchElementException
 
@@ -9,8 +8,8 @@ class Rational (numerator:BigInteger,denominator:BigInteger){
     val normalized: Pair<BigInteger,BigInteger> = normalizeIt()
     var minussign: String
     init {
-        this.minussign = if((rational.first > "0".toBigInteger() && rational.second > "0".toBigInteger())
-            or (rational.first < "0".toBigInteger() && rational.second < "0".toBigInteger() ) ) "" else "-"
+        this.minussign = if((numerator > "0".toBigInteger() && denominator > "0".toBigInteger())
+            or (numerator < "0".toBigInteger() && denominator < "0".toBigInteger() ) ) "" else "-"
     }
     private fun normalizeIt():Pair<BigInteger,BigInteger> {
         var a = "1".toBigInteger()
@@ -25,14 +24,9 @@ class Rational (numerator:BigInteger,denominator:BigInteger){
     }
 
     override fun toString(): String {
-
         return "${this.minussign}" +
-                "${this.rational.first}" +
-                "${if (this.rational.second != 1.toBigInteger()) "/".plus(this.rational.second) else ""}"
-//                "(Normalized version: ${minussign}${Math.abs(normalized.first)}" +
-//                "${if(Math.abs(normalized.second) != 1) "/".plus(Math.abs(normalized.second))
-//                else ""
-
+                "${this.normalized.first}" +
+                "${if (this.normalized.second != 1.toBigInteger()) "/".plus(this.normalized.second) else ""}"
     }
 
     override fun equals(other: Any?): Boolean {
@@ -62,7 +56,7 @@ operator  fun Rational.unaryMinus(): Rational {
     return this
 }
 infix operator fun Rational.compareTo(b: Rational) :Int {
-    return (this.rational.first.toFloat()/this.rational.second.toFloat() ).compareTo(b.rational.first.toFloat()/b.rational.second.toFloat())
+    return (this.rational.first/this.rational.second ).compareTo(b.rational.first/b.rational.second)
 }
 infix operator fun Rational.plus(b:Rational): Rational = Rational(this.rational.first*b.rational.second +
         this.rational.second*b.rational.first,
@@ -88,8 +82,8 @@ fun main() {
     val d:Rational = -21 divBy  105
     println(d.toString() )
 
-   val half = 1 divBy 2
-   val third = 1 divBy 3
+    val half = 1 divBy 2
+    val third = 1 divBy 3
 
     println("half = $half third = $third")
     val sum: Rational = half +  third
@@ -120,11 +114,12 @@ fun main() {
     println("negation operation: $half")
     println(-1 divBy 2 == negation)
 
+    println("String comparesing section :")
     println("(2 divBy 1).toString() == 2 :  ${(2 divBy 1).toString() == "2"}")
-    println(" 2 divBY 1 : ${2 divBy 1}")
-    println(" 2 divBY 1 : ${(2 divBy 1).toString()}")
-    println((-2 divBy 4).toString() == "-1/2")
+    println(" ${-2 divBy 4} == -1/2 is : ${
+        (-2 divBy 4).toString() == "-1/2"}")
     println("117/1098".toRational().toString() == "13/122")
+    println("String comparesing section Ends ! ")
 
     val twoThirds = 2 divBy 3
     println(half < twoThirds)
@@ -139,7 +134,7 @@ fun main() {
 
 //    println(half in third..twoThirds)
 
-    println(2000000000L divBy 4000000000L == 1 divBy 2)
+   // println(2000000000L divBy 4000000000L == 1 divBy 2)
     println(2L divBy 4L == 1 divBy 2)
 
     println("912016490186296920119201192141970416029".toBigInteger() divBy
